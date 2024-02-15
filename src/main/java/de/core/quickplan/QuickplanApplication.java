7dac9d34f652eb7f32698c134e7bebd786be5a4f
deKohle <sebastian.kohler.de@gmail.com>
2024-02-15 18:04:28 +0100

    initial commit

---
+++ src/main/java/de/core/quickplan/QuickplanApplication.java
@@ -0,0 +1,13 @@
+package de.core.quickplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/core/quickplan/QuickplanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.core.quickplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
