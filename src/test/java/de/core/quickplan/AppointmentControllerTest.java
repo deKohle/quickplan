2c89aff7d3259fd3983915d8c2e3f8c14c100c15
Sebastian Kohler <Sebastian.Kohler@vst-pro.de>
2024-04-15 16:35:44 +0200

    added JUnit-Tests

---
+++ src/test/java/de/core/quickplan/AppointmentControllerTest.java
@@ -0,0 +1,67 @@
+package de.core.quickplan;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+//@WebMvcTest
+//@RunWith(SpringRunner.class)
+public class AppointmentControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+	
+	//@MockBean
+	//IAppointmentService appointmentService;
+	
+	private static final String API_URL ="/appointment";
+	
+	@Test
+	public void testAddAppointment() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post(API_URL)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.content(buildUrlEncodedFormEntity("date", "2024-04-15",
+	                "description", "test")))
+		.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+	}
+
+	private String buildUrlEncodedFormEntity(String... params) 
+	{
+		if((params.length % 2) > 0) 
+		{
+			throw new IllegalArgumentException("Need to give an even number of parameters");
+		}
+		    StringBuilder result = new StringBuilder();
+		    for (int i=0; i<params.length; i+=2) 
+		    {
+		        if(i > 0) 
+		        {
+		        	result.append('&');
+		        }
+		        try {
+		            result.
+		            append(URLEncoder.encode(params[i], StandardCharsets.UTF_8.name())).
+		            append('=').
+		            append(URLEncoder.encode(params[i+1], StandardCharsets.UTF_8.name()));
+		        }
+		        catch (UnsupportedEncodingException e) {
+		            throw new RuntimeException(e);
+		        }
+		    }
+		    return result.toString();
+	}
+}
--- src/test/java/de/core/quickplan/QuickplanApplicationTests.java
+++ src/test/java/de/core/quickplan/QuickplanApplicationTests.java
@@ -1,13 +1,21 @@
 package de.core.quickplan;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import de.core.quickplan.controller.AppointmentController;
 
 @SpringBootTest
 class QuickplanApplicationTests {
 
+	@Autowired
+	private AppointmentController controller;
+	
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(controller).isNotNull();
 	}
 
 }
